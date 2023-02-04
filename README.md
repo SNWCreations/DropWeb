3472140694d7f3f52102a6d4fafe0d43ac9aca88
ZX夏夜之风 <snwcreations@qq.com>
2023-02-04 11:27:13 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# DropWeb
+
+Minecraft, but 3x1x3 cobweb blocks will be created behind you when you drop a cobweb item...
+
+Developed for [骑单车の茂茂](https://space.bilibili.com/8957291)
+
+## How it works?
+
+Just drop a cobweb item, it works.
+
+**Tips: the generated cobweb blocks will be replaced to air after 5 seconds.**
+
+## Requirements
+
+A Minecraft server software that implements Bukkit API 1.16.x (Minecraft 1.16.x)
+
+## License
+
+This plugin is licensed under MIT License.
+
+And the additional clause is following:
+
+If you want to create a video about this (or including this), you MUST add this repository link in your video details.
---
+++ src/main/java/snw/dropweb/Main.java
@@ -0,0 +1,119 @@
+package snw.dropweb;
+
+import org.apache.commons.lang.Validate;
+import org.bukkit.*;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public final class Main extends JavaPlugin implements Listener {
+    private static final int MOVE_OFFSET = 3;
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        getServer().getScheduler().cancelTasks(this);
+    }
+
+    @EventHandler
+    public void onDrop(PlayerDropItemEvent e) {
+        Item item = e.getItemDrop();
+        if (item.getItemStack().getType() == Material.COBWEB) {
+            // remove item after 1 tick
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    item.remove(); // goodbye~ lol
+                }
+            }.runTaskLater(this, 1L);
+
+            Player player = e.getPlayer();
+            Location location = player.getLocation();
+            Location basePoint = location.clone();
+            Location lefttop = null; // expected NotNull in the end
+            Location rightdown = null; // expected NotNull in the end
+            float base = location.getYaw();
+
+            if (inRange(base, 135, 225)) {
+                basePoint.add(0, 0, MOVE_OFFSET);
+                lefttop = basePoint.clone().add(-1, 0, 0);
+                rightdown = basePoint.clone().add(1, 0, 2);
+            } else if (inRange(base, 225, 315)) {
+                basePoint.add(-MOVE_OFFSET, 0, 0);
+                lefttop = basePoint.clone().add(0, 0, -1);
+                rightdown = basePoint.clone().add(-2, 0, 1);
+            } else if (
+                    inRange(base, 315, 361) /* Do not think it is strange lol */
+                    || inRange(base, 0, 45)
+            ) {
+                basePoint.add(0, 0, -MOVE_OFFSET);
+                lefttop = basePoint.clone().add(1, 0, 0);
+                rightdown = basePoint.clone().add(-1, 0, -2);
+            } else if (inRange(base, 45, 135)) {
+                basePoint.add(MOVE_OFFSET, 0, 0);
+                lefttop = basePoint.clone().add(0, 0, 1);
+                rightdown = basePoint.clone().add(2, 0, -1);
+            }
+
+            // location.getWorld().setGameRule(GameRule.SEND_COMMAND_FEEDBACK, false);
+
+            // fill
+            int fill = fill(lefttop, rightdown, Material.AIR, Material.COBWEB);
+            if (fill == 0) {
+                return; // do nothing lol
+            }
+
+            // fill back
+            Location finalRightdown = rightdown;
+            Location finalLefttop = lefttop;
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    fill(finalLefttop, finalRightdown, Material.COBWEB, Material.AIR);
+                }
+            }.runTaskLater(this, 20L * 5);
+        }
+    }
+
+    private static boolean inRange(float source, float a, float b) {
+        return source >= a && source < b;
+    }
+
+    private static int fill(Location lefttop, Location rightdown, Material from, Material to) {
+        Validate.isTrue(lefttop.getWorld() != null, "world cannot be null");
+        Validate.isTrue(rightdown.getWorld() != null, "world cannot be null");
+        Validate.isTrue(lefttop.getWorld() == rightdown.getWorld(), "Cannot fill in different world");
+
+        int successCount = 0;
+        World world = lefttop.getWorld();
+        int x1 = Math.min(lefttop.getBlockX(), rightdown.getBlockX());
+        int x2 = Math.max(lefttop.getBlockX(), rightdown.getBlockX());
+        int y1 = Math.min(lefttop.getBlockY(), rightdown.getBlockY());
+        int y2 = Math.max(lefttop.getBlockY(), rightdown.getBlockY());
+        int z1 = Math.min(lefttop.getBlockZ(), rightdown.getBlockZ());
+        int z2 = Math.max(lefttop.getBlockZ(), rightdown.getBlockZ());
+        for (int x = x1; x <= x2; x++) {
+            for (int y = y1; y <= y2; y++) {
+                for (int z = z1; z <= z2; z++) {
+                    Block block = world.getBlockAt(x, y, z);
+                    if (block.getType() == from) {
+                        block.setType(to);
+                        successCount++;
+                    }
+                }
+            }
+        }
+        return successCount;
+    }
+}
